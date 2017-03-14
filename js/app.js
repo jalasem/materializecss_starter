11c41bb90648e7fa5837aabdd2d31aefcaf73e20
ABDUL-SAMII AJALA <ABDUL-SAMII AJALA>
2017-03-14 05:10:44 +0100

    initialized sidenav

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.button-collapse').sideNav({
+      menuWidth: 250, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+})
